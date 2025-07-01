7d039ebaa19b7cabbcae0e7c54941dff318e5eb4
Diegoorlanzx <155659029+Diegoorlanzx@users.noreply.github.com>
2025-06-30 19:05:25 -0500

    Primer commit - Proyecto Catálogo de Películas

---
+++ src/presentacion/CatalogoPeliculasApp.java
@@ -0,0 +1,65 @@
+package presentacion;
+
+import dominio.Pelicula;
+import servicio.IServicioPeliculas;
+import servicio.ServicioPeliculasLista;
+
+import java.util.Scanner;
+
+public class CatalogoPeliculasApp {
+
+    public static void main(String[] args) {
+        var salir = false;
+        var consola = new Scanner(System.in);
+        IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+
+        while (!salir) {
+            try {
+                mostrarMenu();
+                salir = ejecutarOpciones(consola, servicioPeliculas);
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error: " + e.getMessage());
+            }
+            System.out.println("\n");
+        }
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("""
+                *** Catálogo de Películas ***
+                1. Agregar Película
+                2. Listar Películas
+                3. Buscar Película
+                4. Salir
+                """);
+    }
+
+    // Método pendiente de implementar
+    private static boolean ejecutarOpciones(Scanner consola, IServicioPeliculas servicioPeliculas) {
+        // Implementación aquí
+        var opcion = Integer.parseInt(consola.nextLine());
+       var salir = false;
+       switch (opcion){
+           case 1 -> {
+               System.out.println("Introduce el nombre de la pelicula");
+               var nombrepelicula = consola.nextLine();
+               servicioPeliculas.agregarPelicula(new Pelicula(nombrepelicula));
+;           }
+           case 2 -> {
+               servicioPeliculas.listarPeliculas();
+           }
+           case 3 -> {
+               System.out.println("Introduce el nombre de la pelicula a buscar");
+               var buscarPelicula = consola.nextLine();
+               servicioPeliculas.buscarPeliculas(new Pelicula(buscarPelicula));
+           }
+           case 4 -> {
+               System.out.println("Hasta pronto");
+               salir = true;
+           }
+           default -> System.out.println("Opcion no reconociddda : " + opcion);
+       }
+       return salir;
+    }
+}
+
---
+++ src/servicio/ServicioPeliculasLista.java
@@ -0,0 +1,51 @@
+package servicio;
+
+import dominio.Pelicula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioPeliculasLista implements IServicioPeliculas{
+    private final List<Pelicula> peliculas;
+
+    public  ServicioPeliculasLista(){
+        this.peliculas = new ArrayList<>();
+    }
+
+    @Override
+    public void listarPeliculas() {
+        System.out.println("Listado De Peliculas... ");
+        peliculas.forEach(System.out::println);
+
+    }
+
+    @Override
+    public void agregarPelicula(Pelicula pelicula) {
+        peliculas.add(pelicula);
+        System.out.println("Se agrego la pelicula : " + pelicula);
+
+    }
+
+    @Override
+    public void buscarPeliculas(Pelicula pelicula) {
+        // regresar indice de la pelicuila encontrada
+        var indice = peliculas.indexOf(pelicula);
+        if (indice == -1)
+            System.out.println("No se encontro la pelicula " + pelicula);
+        else
+            System.out.println("Pelicula encontrada en el indice :" + indice);
+
+    }
+
+    public static void main(String[] args) {
+       // var pelicula1 = new Pelicula("batman");
+        //var pelicula2 = new Pelicula("superman");
+       // IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+
+       // servicioPeliculas.agregarPelicula(pelicula1);
+       // servicioPeliculas.agregarPelicula(pelicula2);
+
+        //servicioPeliculas.buscarPeliculas(pelicula1);
+
+    }
+}
